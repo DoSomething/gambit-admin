8bc7e6a993f233bb3cc42e0a99c94933a3ec1c47
Aaron Schachter <aaronschachter@gmail.com>
2017-10-26 13:13:28 -0700

    Adds auth to Conversations requests

--- config/gambit-conversations.js
+++ config/gambit-conversations.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const configVars = {
+  auth: {
+    name: process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_NAME,
+    pass: process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_PASS,
+  },
   baseUri: process.env.DS_GAMBIT_CONVERSATIONS_API_BASEURI,
 };
 
