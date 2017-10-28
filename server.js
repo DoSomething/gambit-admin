29fe97310ac18604f55d21713d246b055729bddb
Aaron Schachter <aaronschachter@gmail.com>
2017-10-28 13:55:08 -0700

    Adds heroku-postbuild

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const logger = require('heroku-logger');
 
 const app = express();
 
 require('dotenv').config()
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+const buildPath = 'client/build';
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, buildPath)));
 
 require('./routes')(app);
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, buildPath, 'index.html'));
+});
 
 app.listen(app.get('port'), () => {
   logger.info(`Gambit Admin server running at: http://localhost:${app.get('port')}/`);
