dafecc0c967e36d17229adfa4ab067b8e55454b0
Aaron Schachter <aaronschachter@gmail.com>
2017-11-27 14:24:24 -0800

    Adds Conversation Signups view

--- client/src/Components/ConversationDetail.js
+++ client/src/Components/ConversationDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Request from 'react-http-request';
-import { Col, Panel, Grid, Label, PageHeader, Row } from 'react-bootstrap';
+import { Col, Panel, Grid, Image, Label, ListGroup, ListGroupItem, PageHeader, Row, Tab, Table, Tabs} from 'react-bootstrap';
 import Moment from 'react-moment';
 import MessageList from './MessageList';
 import RequestError from './RequestError';
@@ -40,6 +40,89 @@ export default class ConversationDetail extends React.Component {
     );
   }
 
+  renderNav(conversation) {
+    const numSignups = conversation.user.signups.meta.pagination.total;
+    const signupsLabel = `Signups (${numSignups})`;
+
+    return (
+      <Tabs defaultActiveKey={0} animation={false} id="campaign-tabs">
+        <Tab eventKey={0} title="Messages"><br />
+           <MessageList conversationId={this.conversationId} />
+        </Tab>
+        <Tab eventKey={1} title={signupsLabel}><br />
+          { this.renderSignups(conversation.user.signups) }
+        </Tab>
+      </Tabs>
+    );
+  }
+
+  renderSignup(signup) {
+    let createdAt = <Moment format={config.dateFormat}>{signup.created_at}</Moment>;
+    let signupSource = null;
+    if (signup.signup_source) {
+      signupSource = <p>{signup.signup_source}</p>;
+    }
+    let posts = null;
+    const numPosts = signup.posts.data.length;
+    if (numPosts) {
+      posts = signup.posts.data.map((post, index) => {
+        console.log(post);
+        let whyParticipated = null;
+        if (index === numPosts - 1) {
+          whyParticipated = (
+            <ListGroupItem>
+              <label>Why Participated:</label> {signup.why_participated}
+            </ListGroupItem>
+          );
+        }
+        return (
+          <ListGroup key={post.id}>
+            <ListGroupItem>
+              <Image src={post.media.url} height={200} />
+            </ListGroupItem>
+            <ListGroupItem>
+              <label>Caption:</label> {post.media.caption}
+            </ListGroupItem>
+            <ListGroupItem>
+              <label>Quantity:</label> {signup.quantity}
+            </ListGroupItem>
+            {whyParticipated}
+            <ListGroupItem>
+              <label>Status:</label> {post.status}
+            </ListGroupItem>
+          </ListGroup>
+        );
+      });
+    }
+    return (
+      <tr key={signup.signup_id}>
+        <td>{signup.signup_id}</td>
+        <td>{signup.campaign_id}</td>
+        <td>{createdAt} {signupSource}</td>
+        <td>{posts}</td>
+      </tr>
+    );
+  }
+
+  renderSignups(signups) {
+    const rows = signups.data.map(signup => this.renderSignup(signup));
+    return (
+      <Table>
+      <thead>
+        <tr>
+          <th width={100}>Signup</th>
+          <th width={100}>Campaign</th>
+          <th width={200}>Created</th>
+          <th>Posts</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+      </Table>
+    );
+  }
+
   renderDetail(conversation) {
     let label;
     if (conversation.paused) {
@@ -50,7 +133,7 @@ export default class ConversationDetail extends React.Component {
       <Grid>
         <PageHeader>{ conversation.platformUserId } {label}</PageHeader>
         {conversation.user ? this.renderUser(conversation.user) : ''}
-        <MessageList conversationId={this.conversationId} />
+        {this.renderNav(conversation)}
       </Grid>
     );
   }
