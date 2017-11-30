b37df5dbff3ca320e0d1df730874c4b6a0a34584
Aaron Schachter <aaronschachter@gmail.com>
2017-11-29 19:42:50 -0800

    Lints MessageList

--- client/src/Components/MessageList.js
+++ client/src/Components/MessageList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Request from 'react-http-request';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Col, Grid, Row, ListGroup, ListGroupItem, Image, Pager } from 'react-bootstrap';
 import Moment from 'react-moment';
@@ -9,95 +9,10 @@ const queryString = require('query-string');
 const config = require('../config');
 const helpers = require('../helpers');
 
-const pageSize = config.resultsPageSize; 
+const pageSize = config.resultsPageSize;
 
-export default class MessageList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const apiQuery = {
-      sort: '-createdAt',
-      limit: pageSize,
-      populate: 'conversationId',
-    }
-
-    if (this.props.campaignId) {
-      apiQuery.query = `{"campaignId":"${this.props.campaignId}"}`;
-    } else if (this.props.conversationId) {
-      apiQuery.query = `{"conversationId":"${this.props.conversationId}"}`;
-    } else if (this.props.requestId) {
-      apiQuery.query = `{"metadata.requestId":"${this.props.requestId}"}`;
-    }
-
-    // Check for skip query string parameter.
-    const clientQuery = queryString.parse(window.location.search);
-    this.skipCount = Number(clientQuery.skip);
-    if (this.skipCount) {
-      apiQuery.skip = this.skipCount;
-    } else {
-      this.skipCount = 0;
-    }
-
-    this.requestUrl = helpers.getMessagesUrl(apiQuery);
-  }
-
-  renderTablePager(totalResultCount, pageCount) {
-    const startNumber = this.skipCount + 1;
-    const endNumber = startNumber + pageCount - 1;
-
-    const location = window.location;
-    const url = [location.protocol, '//', location.host, location.pathname].join('');
-
-    let leftPagerItem;
-    if (this.skipCount) {
-      const prevSkip = this.skipCount - pageSize;
-      const prevUrl = `${url}?skip=${prevSkip}`;
-      leftPagerItem = <Pager.Item previous href={prevUrl}>Previous</Pager.Item>;
-    }
-
-    let rightPagerItem;
-    if (totalResultCount > pageSize && endNumber < totalResultCount) {
-      const nextUrl = `${url}?skip=${this.skipCount + pageSize}`;
-      rightPagerItem = <Pager.Item next href={nextUrl}>Next</Pager.Item>;
-    }
-    let label = 'No results';
-    if (totalResultCount > 0) {
-      label = `${startNumber}-${endNumber} of ${totalResultCount}`;
-    }
-
-    return (
-      <Pager>
-        {leftPagerItem}
-        <small>{label}</small>
-        {rightPagerItem}
-      </Pager>
-    );
-  }
-
-  render() {
-    return (
-      <HttpRequest url={ this.requestUrl }>
-        {
-          res => {
-            const totalCount = res.pagination.total;
-            const pageSize = res.data.length;
-            const pager = this.renderTablePager(totalCount, pageSize);
-            return (
-              <div>
-                { pager }
-                <Grid>
-                  { res.data.map(message => this.renderMessageRow(message)) }
-                </Grid>
-                { totalCount > 10 ? pager : null }
-              </div>
-            );
-          }
-        }
-      </HttpRequest>
-    );
-  }
-
-  renderDate(message) {
+class MessageList extends React.Component {
+  static renderDate(message) {
     const dateFormat = config.dateFormat;
     const createdAt = <Moment format={dateFormat}>{ message.createdAt }</Moment>;
     if (!message.metadata) {
@@ -111,13 +26,13 @@ export default class MessageList extends React.Component {
     );
   }
 
-  renderMessageContent(message) {
+  static renderMessageContent(message) {
     let text = message.text;
     if (message.direction === 'inbound') {
       text = <strong>{ text }</strong>;
     }
     let broadcastGroupItem = null;
-    let broadcastId = message.broadcastId;
+    const broadcastId = message.broadcastId;
     if (broadcastId) {
       broadcastGroupItem = (
         <ListGroupItem>
@@ -125,7 +40,7 @@ export default class MessageList extends React.Component {
         </ListGroupItem>
       );
     }
-    let attachments = message.attachments;
+    const attachments = message.attachments;
     let attachmentGroupItem = null;
     if (attachments.length) {
       attachmentGroupItem = (
@@ -199,20 +114,118 @@ export default class MessageList extends React.Component {
     );
   }
 
-  renderMessageRow(message) {
+  static renderMessageRow(message) {
     const uri = `/conversations/${message.conversationId._id}`;
-    const userLink = <Link to={uri}>{ message.conversationId.platformUserId }</Link>;
+    const userLink = <Link to={uri}>{message.conversationId.platformUserId}</Link>;
     const isInbound = message.direction === 'inbound';
     const offset = isInbound ? 0 : 1;
 
     return (
-      <Row key={ message._id }>
+      <Row key={message._id}>
         <Col md={2} mdOffset={2}>
           <div>{ isInbound ? 'From' : 'To' } <strong>{userLink}</strong></div>
-          <small>{ this.renderDate(message) }</small>
+          <small>{ MessageList.renderDate(message) }</small>
         </Col>
-        <Col md={4} mdOffset={offset}>{ this.renderMessageContent(message) }</Col>
+        <Col md={4} mdOffset={offset}>{ MessageList.renderMessageContent(message) }</Col>
       </Row>
     );
   }
+
+  constructor(props) {
+    super(props);
+
+    const apiQuery = {
+      sort: '-createdAt',
+      limit: pageSize,
+      populate: 'conversationId',
+    };
+
+    if (this.props.campaignId) {
+      apiQuery.query = `{"campaignId":"${this.props.campaignId}"}`;
+    } else if (this.props.conversationId) {
+      apiQuery.query = `{"conversationId":"${this.props.conversationId}"}`;
+    } else if (this.props.requestId) {
+      apiQuery.query = `{"metadata.requestId":"${this.props.requestId}"}`;
+    }
+
+    // Check for skip query string parameter.
+    const clientQuery = queryString.parse(window.location.search);
+    this.skipCount = Number(clientQuery.skip);
+    if (this.skipCount) {
+      apiQuery.skip = this.skipCount;
+    } else {
+      this.skipCount = 0;
+    }
+
+    this.requestUrl = helpers.getMessagesUrl(apiQuery);
+  }
+
+  renderTablePager(totalResultCount, pageCount) {
+    const startNumber = this.skipCount + 1;
+    const endNumber = (startNumber + pageCount) - 1;
+
+    const location = window.location;
+    const url = [location.protocol, '//', location.host, location.pathname].join('');
+
+    let leftPagerItem;
+    if (this.skipCount) {
+      const prevSkip = this.skipCount - pageSize;
+      const prevUrl = `${url}?skip=${prevSkip}`;
+      leftPagerItem = <Pager.Item previous href={prevUrl}>Previous</Pager.Item>;
+    }
+
+    let rightPagerItem;
+    if (totalResultCount > pageSize && endNumber < totalResultCount) {
+      const nextUrl = `${url}?skip=${this.skipCount + pageSize}`;
+      rightPagerItem = <Pager.Item next href={nextUrl}>Next</Pager.Item>;
+    }
+    let label = 'No results';
+    if (totalResultCount > 0) {
+      label = `${startNumber}-${endNumber} of ${totalResultCount}`;
+    }
+
+    return (
+      <Pager>
+        {leftPagerItem}
+        <small>{label}</small>
+        {rightPagerItem}
+      </Pager>
+    );
+  }
+
+  render() {
+    return (
+      <HttpRequest url={this.requestUrl}>
+        {
+          (res) => {
+            const totalCount = res.pagination.total;
+            const pager = this.renderTablePager(totalCount, res.data.length);
+            return (
+              <div>
+                { pager }
+                <Grid>
+                  { res.data.map(message => MessageList.renderMessageRow(message)) }
+                </Grid>
+                { totalCount > 10 ? pager : null }
+              </div>
+            );
+          }
+        }
+      </HttpRequest>
+    );
+  }
 }
+
+MessageList.propTypes = {
+  campaignId: PropTypes.string,
+  conversationId: PropTypes.string,
+  requestId: PropTypes.string,
+};
+
+MessageList.defaultProps = {
+  campaignId: null,
+  conversationId: null,
+  requestId: null,
+};
+
+export default MessageList;
