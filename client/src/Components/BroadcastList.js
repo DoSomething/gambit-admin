deaf15cd6e5e45cdce867876d7beb64294d8139b
Aaron Schachter <aaronschachter@gmail.com>
2017-12-03 22:15:10 -0800

    Adds Conversations, Broadcasts nav items to Header

---
+++ client/src/Components/BroadcastList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Grid, Image, PageHeader} from 'react-bootstrap';
+
+const BroadcastList = () => (
+  <Grid>
+    <PageHeader>Broadcasts</PageHeader>
+    <h2>Under construction</h2>
+    <Image src="http://textfiles.com/underconstruction/CaCapeCanaveralHangar2256construction.gif" />
+  </Grid>
+);
+
+
+export default BroadcastList;
