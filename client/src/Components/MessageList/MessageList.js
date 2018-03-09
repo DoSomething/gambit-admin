e728a5892f2369b22b01c7b13ad4887e0649b8e3
Aaron Schachter <aaronschachter@gmail.com>
2018-03-08 21:35:34 -0800

    Adds ListPager

--- client/src/Components/MessageList/MessageList.js
+++ client/src/Components/MessageList/MessageList.js
@@ -1,46 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Pager, Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import MessageListItem from './MessageListItem';
-
-function renderPager(totalResultCount, skipCount, pageCount, pageSize) {
-  const startNumber = skipCount + 1;
-  const endNumber = (startNumber + pageCount) - 1;
-
-  const location = window.location;
-  const url = [location.protocol, '//', location.host, location.pathname].join('');
-
-  let leftPagerItem;
-  if (skipCount) {
-    const prevSkip = skipCount - pageSize;
-    const prevUrl = `${url}?skip=${prevSkip}`;
-    leftPagerItem = <Pager.Item previous href={prevUrl}>Previous</Pager.Item>;
-  }
-
-  let rightPagerItem;
-  if (totalResultCount > pageSize && endNumber < totalResultCount) {
-    const nextUrl = `${url}?skip=${skipCount + pageSize}`;
-    rightPagerItem = <Pager.Item next href={nextUrl}>Next</Pager.Item>;
-  }
-  let label = 'No results';
-  if (totalResultCount > 0) {
-    label = `${startNumber}-${endNumber} of ${totalResultCount.toLocaleString()}`;
-  }
-
-  return (
-    <Pager>
-      {leftPagerItem}
-      <small>{label}</small>
-      {rightPagerItem}
-    </Pager>
-  );
-}
+import ListPager from '../ListPager';
 
 const MessageList = (props) => {
   if (!props.data) {
     return <div>No results found.</div>;
   }
-  const pager = renderPager(props.totalCount, props.skipCount, props.data.length, props.pageSize);
+  const pager = (
+    <ListPager
+      totalResultCount={props.totalCount}
+      skipCount={props.skipCount}
+      pageCount={props.data.length}
+      pageSize={props.pageSize}
+      description="messages"
+    />
+  );
   let data = props.data.map(message => <MessageListItem table={props.table} message={message} />);
   if (props.table) {
     data = <Table><tbody>{data}</tbody></Table>;
