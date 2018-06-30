b7547f2865d68bac4782eed12e86e2258829f0c7
Aaron Schachter <aaronschachter@gmail.com>
2018-06-29 20:03:05 -0700

    Adds ScrollableAnchor to each TriggerListItem

--- client/src/Components/TriggerList/TriggerListItem.js
+++ client/src/Components/TriggerList/TriggerListItem.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
+import ScrollableAnchor from 'react-scrollable-anchor';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ContentfulLink from '../ContentfulLink';
 
+/**
+ * @param {Object} trigger
+ * @return {String}
+ */
 function getResponseFromTrigger(trigger) {
   if (trigger.topicId) {
     const url = `/topics/${trigger.topicId}`;
     return <Link to={url}>{trigger.topic.name}</Link>;
   }
   if (trigger.redirect) {
-    return `@ ${trigger.redirect}`;
+    return <a href={`#${encodeURIComponent(trigger.redirect)}`}>{`@ ${trigger.redirect}`}</a>;
   }
   return trigger.reply;
 }
 
 const TriggerListItem = (props) => {
   const trigger = props.trigger;
+  const triggerText = trigger.trigger;
+  const anchor = encodeURIComponent(triggerText);
   return (
     <tr><td>
-      <Row key={trigger.id}>
-        <Col md={3}><strong>{trigger.trigger}</strong></Col>
-        <Col md={8}>{getResponseFromTrigger(trigger)}</Col>
-        <Col md={1}>
-          <ContentfulLink bsStyle="link" bsSize="xsmall" entryId={trigger.id} />
-        </Col>
-      </Row>
+      <ScrollableAnchor id={anchor}>
+        <Row key={trigger.id}>
+          <Col md={3}>
+            <a href={`#${anchor}`}><strong>{triggerText}</strong></a>
+          </Col>
+          <Col md={8}>{getResponseFromTrigger(trigger)}</Col>
+          <Col md={1}>
+            <ContentfulLink bsStyle="link" bsSize="xsmall" entryId={trigger.id} />
+          </Col>
+        </Row>
+      </ScrollableAnchor>
     </td></tr>
   );
 };
