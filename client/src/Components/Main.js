cab227bbdc23300e6fd3abb4d1fb205c796d56ec
Aaron Schachter <aaronschachter@gmail.com>
2018-07-02 20:32:38 -0700

    Refactors Switch per https://github.com/DoSomething/gambit-admin/pull/59/files#r199632900

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -3,7 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
 import CampaignDetail from './CampaignDetail/CampaignDetailContainer';
-import ConversationList from './ConversationList/ConversationListContainer';
+// TODO: Rename ConversationList as UserList
+import UserList from './ConversationList/ConversationListContainer';
 import UserDetail from './UserDetail/UserDetailContainer';
 import ConversationRequest from './ConversationRequest';
 import BroadcastList from './BroadcastList/BroadcastListContainer';
@@ -13,62 +14,24 @@ import TopicDetail from './TopicDetail/TopicDetailContainer';
 import TopicList from './TopicList/TopicListContainer';
 import TriggerList from './TriggerList/TriggerListContainer';
 
-const Broadcasts = () => (
-  <Switch>
-    <Route exact path="/broadcasts" component={BroadcastList} />
-    <Route path="/broadcasts/:broadcastId/:macro" component={BroadcastReplyList} />
-    <Route path="/broadcasts/:broadcastId" component={BroadcastDetail} />
-  </Switch>
-);
-
-const Campaigns = () => (
-  <Switch>
-    <Route exact path="/campaigns" component={TopicList} />
-    <Route path="/campaigns/:campaignId" component={CampaignDetail} />
-  </Switch>
-);
-
-const ConversationsRequests = () => (
-  <Switch>
-    <Route exact path="/requests" component={Home} />
-    <Route path="/requests/:requestId" component={ConversationRequest} />
-  </Switch>
-);
-
-const Topics = () => (
-  <Switch>
-    <Route exact path="/topics" component={TopicList} />
-    <Route path="/topics/:topicId" component={TopicDetail} />
-  </Switch>
-);
-
-
-const Triggers = () => (
-  <Switch>
-    <Route exact path="/triggers" component={TriggerList} />
-  </Switch>
-);
-
-const Users = () => (
-  <Switch>
-    <Route exact path="/users" component={ConversationList} />
-    <Route path="/users/:userId" component={UserDetail} />
-  </Switch>
-);
-
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/broadcasts" component={Broadcasts} />
-      <Route path="/campaigns" component={Campaigns} />
-      <Route path="/requests" component={ConversationsRequests} />
-      <Route path="/topics" component={Topics} />
-      <Route path="/triggers" component={Triggers} />
-      <Route path="/users" component={Users} />
+      <Route path="/broadcasts/:broadcastId/:macro" component={BroadcastReplyList} />
+      <Route path="/broadcasts/:broadcastId" component={BroadcastDetail} />
+      <Route path="/broadcasts" component={BroadcastList} />
+      <Route path="/campaigns/:campaignId" component={CampaignDetail} />
+      <Route path="/campaigns" component={TopicList} />
+      <Route path="/requests/:requestId" component={ConversationRequest} />
+      <Route path="/requests" component={Home} />
+      <Route path="/topics/:topicId" component={TopicDetail} />
+      <Route path="/topics" component={TopicList} />
+      <Route path="/triggers" component={TriggerList} />
+      <Route path="/users/:userId" component={UserDetail} />
+      <Route path="/users" component={UserList} />
+      <Route path="/" component={Home} />
     </Switch>
   </main>
 );
 
 export default Main;
-
