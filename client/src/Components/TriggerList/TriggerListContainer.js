987b0485a2589a77bcfc2cf5d40f5b1dd87c7867
Aaron Schachter <aaronschachter@gmail.com>
2018-09-04 11:35:11 -0700

    Adds link to clear Rivescript cache

--- client/src/Components/TriggerList/TriggerListContainer.js
+++ client/src/Components/TriggerList/TriggerListContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Col, Grid, Row, Table } from 'react-bootstrap';
+import { Col, Glyphicon, Grid, Row, Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import queryString from 'query-string';
 import lodash from 'lodash';
 import HttpRequest from '../HttpRequest';
 import TriggerListItem from './TriggerListItem';
@@ -7,18 +9,28 @@ import helpers from '../../helpers';
 
 const TriggerListContainer = () => (
   <Grid>
-    <HttpRequest path={helpers.getRivescriptPath()}>
+    <HttpRequest
+      path={helpers.getRivescriptPath()}
+      query={queryString.parse(window.location.search)}
+    >
       {res => (
-        <Table striped><tbody>
-          <tr><th>
-            <Row key="header">
-              <Col md={3}>Trigger</Col>
-              <Col md={9}>Reply</Col>
-            </Row>
-          </th></tr>
-          {lodash.orderBy(res.data.topics.random, 'trigger')
-            .map(trigger => (<TriggerListItem trigger={trigger} />))}
-        </tbody></Table>
+        <div>
+          <p>
+            <Link to="?cache=false">
+              <Glyphicon glyph="refresh" /> Refresh
+            </Link>
+          </p>
+          <Table striped><tbody>
+            <tr><th>
+              <Row key="header">
+                <Col md={3}>Trigger</Col>
+                <Col md={9}>Reply</Col>
+              </Row>
+            </th></tr>
+            {lodash.orderBy(res.data.topics.random, 'trigger')
+              .map(trigger => (<TriggerListItem key={trigger.trigger} trigger={trigger} />))}
+          </tbody></Table>
+        </div>
       )}
     </HttpRequest>
   </Grid>
