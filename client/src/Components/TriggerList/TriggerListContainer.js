4f1a66e2a0bd37837cca53ad86da640359d6f804
Aaron Schachter <aaronschachter@gmail.com>
2018-09-04 12:33:43 -0700

    Adds Panel

--- client/src/Components/TriggerList/TriggerListContainer.js
+++ client/src/Components/TriggerList/TriggerListContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Col, Glyphicon, Grid, Row, Table } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Button, ButtonToolbar, Col, Glyphicon, Grid, Panel, Row, Table } from 'react-bootstrap';
 import queryString from 'query-string';
 import lodash from 'lodash';
 import HttpRequest from '../HttpRequest';
@@ -13,25 +12,33 @@ const TriggerListContainer = () => (
       path={helpers.getRivescriptPath()}
       query={queryString.parse(window.location.search)}
     >
-      {res => (
-        <div>
-          <p>
-            <Link to="?cache=false">
-              <Glyphicon glyph="refresh" /> Refresh
-            </Link>
-          </p>
-          <Table striped><tbody>
-            <tr><th>
-              <Row key="header">
-                <Col md={3}>Trigger</Col>
-                <Col md={9}>Reply</Col>
-              </Row>
-            </th></tr>
-            {lodash.orderBy(res.data.topics.random, 'trigger')
-              .map(trigger => (<TriggerListItem key={trigger.trigger} trigger={trigger} />))}
-          </tbody></Table>
-        </div>
-      )}
+      {(res) => {
+        const rows = lodash.orderBy(res.data.topics.random, 'trigger')
+          .map(trigger => (<TriggerListItem key={trigger.trigger} trigger={trigger} />));
+        return (
+          <div>
+            <Panel>
+              <Panel.Body>
+                <p>Found <strong>{rows.length}</strong> triggers loaded.</p>
+                <ButtonToolbar>
+                  <Button type="reset" href="?cache=false" bsSize="small">
+                    <Glyphicon glyph="refresh" />
+                  </Button>
+                </ButtonToolbar>
+              </Panel.Body>
+            </Panel>
+            <Table striped><tbody>
+              <tr><th>
+                <Row key="header">
+                  <Col md={3}>Trigger</Col>
+                  <Col md={9}>Reply</Col>
+                </Row>
+              </th></tr>
+              {rows}
+            </tbody></Table>
+          </div>
+        );
+      }}
     </HttpRequest>
   </Grid>
 );
