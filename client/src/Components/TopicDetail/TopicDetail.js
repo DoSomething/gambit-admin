385235edb08500e34f0352e97134cceb3a36c87e
Aaron Schachter <aaronschachter@gmail.com>
2018-10-29 10:03:50 -0700

    Removes unused topic description

--- client/src/Components/TopicDetail/TopicDetail.js
+++ client/src/Components/TopicDetail/TopicDetail.js
@@ -3,17 +3,10 @@ import { Panel, Grid, PageHeader } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import TemplateList from '../TemplateList/TemplateList';
 import ContentfulLink from '../ContentfulLink';
-import helpers from '../../helpers';
 
 const TopicDetail = (props) => {
   const topic = props.topic;
-  const html = helpers.getTopicDescription(topic);
-  const summary = (
-    <span
-      dangerouslySetInnerHTML={{ __html: html }} // eslint-disable-line react/no-danger
-    />
-  );
-
+  const campaignTitle = topic.campaign.title ? topic.campaign.title : '(None)';
   return (
     <Grid>
       <PageHeader>{topic.name}</PageHeader>
@@ -21,7 +14,7 @@ const TopicDetail = (props) => {
         <Panel.Body>
           <ContentfulLink entryId={topic.id} />
           <p>
-            <strong>Campaign</strong>: {topic.campaign.title}
+            <strong>Campaign</strong>: {campaignTitle}
           </p>
         </Panel.Body>
       </Panel>
