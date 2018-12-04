91956d3f40c08487f17b95fb7502e7abea448eca
Aaron Schachter <aaronschachter@gmail.com>
2018-12-04 10:27:29 -0800

    Adds Users dropdown to link to DraftSubmissions

--- client/src/Components/DraftSubmissionDetail/DraftSubmissionDetail.js
+++ client/src/Components/DraftSubmissionDetail/DraftSubmissionDetail.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Panel } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
 import DraftSubmissionValues from './DraftSubmissionValues';
+import config from '../../config';
 
 const DraftSubmissionDetail = (props) => {
   const draft = props.draftSubmission;
+  const topicId = draft.topicId;
+  const userId = draft.conversationId.userId;
   return (
     <div>
       <Panel>
         <Panel.Body>
           <p>
-            <strong>User:</strong> {draft.conversationId.userId}
+            <Moment format={config.dateFormat}>{draft.createdAt}</Moment>
           </p>
           <p>
-            <strong>Topic:</strong> {draft.topicId}
+            <strong>User:</strong> <Link to={`/users/${userId}`}>{userId}</Link>
+          </p>
+          <p>
+            <strong>Topic:</strong> <Link to={`/topics/${topicId}`}>{topicId}</Link>
           </p>
           <DraftSubmissionValues draftSubmission={draft} />
         </Panel.Body>
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, MenuItem, NavItem } from 'react-bootstrap';
 import ConversationSearchForm from './ConversationSearchForm';
 
 class Header extends React.Component {
   render() {
     const pathname = window.location.pathname;
-    const activeUsers = (pathname.includes('users') || pathname.includes('requests'));
+    const activeUsers = (pathname.includes('users') || pathname.includes('requests') || pathname.includes('draft'));
     const activeCampaigns = (pathname.includes('campaigns') || pathname.includes('topics'));
     return (
       <Navbar>
@@ -16,9 +16,10 @@ class Header extends React.Component {
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          <NavItem active={activeUsers} eventKey={1} href="/users">
-            Users
-          </NavItem>
+          <NavDropdown active={activeUsers} eventKey={1} title="Users" id="users-nav-dropdown">
+            <MenuItem eventKey={1.1} href="/users">Conversation last updated</MenuItem>
+            <MenuItem eventKey={1.2} href="/draftSubmissions">Draft photo posts</MenuItem>
+          </NavDropdown>
           <NavItem active={activeCampaigns} eventKey={2} href="/campaigns">
             Campaigns
           </NavItem>
