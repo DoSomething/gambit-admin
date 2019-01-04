bb37f6d20e20020b6c8bffe6fb1f1d662d898146
Aaron Schachter <aaronschachter@gmail.com>
2019-01-04 06:42:37 -0800

    Adds northstar to config/services

--- config/app.js
+++ config/app.js
@@ -1,8 +1,11 @@
 'use strict';
 
 const configVars = {
+  appUrl: process.env.APP_URL,
   buildPath: 'client/build',
   port: process.env.PORT || 3000,
+  // Only used when running client as a separate server (local development).
+  webUrl: process.env.WEB_URL,
 };
 
 module.exports = configVars;
--- config/services.js
+++ config/services.js
@@ -1,6 +1,13 @@
 'use strict';
 
 module.exports = {
+  gambitContent: {
+    baseUri: process.env.DS_GAMBIT_CAMPAIGNS_API_BASEURI,
+    auth: {
+      header: 'x-gambit-api-key',
+      key: process.env.DS_GAMBIT_CAMPAIGNS_API_KEY,
+    },
+  },
   gambitConversations: {
     auth: {
       name: process.env.DS_GAMBIT_CONVERSATIONS_API_BASIC_AUTH_NAME,
@@ -10,15 +17,13 @@ module.exports = {
     // @see https://github.com/DoSomething/gambit-conversations/blob/7bab3025e38630b938cba844c6755bba5c2373e7/config/app/routes/mongoose.js#L4
     resultsCountHeader: 'x-gambit-results-count',
   },
-  gambitContent: {
-    baseUri: process.env.DS_GAMBIT_CAMPAIGNS_API_BASEURI,
-    auth: {
-      header: 'x-gambit-api-key',
-      key: process.env.DS_GAMBIT_CAMPAIGNS_API_KEY,
-    },
+  graphQL: {
+    url: process.env.DS_GRAPHQL_URL,
   },
-  // TODO: Add OAuth vars.
   northstar: {
-
+    url: process.env.DS_API_OAUTH_URL,
+    clientId: process.env.DS_API_OAUTH_CLIENT_ID,
+    clientSecret: process.env.DS_API_OAUTH_CLIENT_SECRET,
+    scopes: process.env.DS_API_OAUTH_SCOPES,
   },
 };
