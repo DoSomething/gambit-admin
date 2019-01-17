767f2c4b78d05aafb94bff5308edd43ffc86dc8d
Aaron Schachter <aaronschachter@gmail.com>
2019-01-17 14:40:10 -0800

    Refactors TemplateList

--- client/src/Components/TopicDetail/TopicDetailContainer.js
+++ client/src/Components/TopicDetail/TopicDetailContainer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, PageHeader } from 'react-bootstrap';
-import queryString from 'query-string';
+import { Grid } from 'react-bootstrap';
 import GraphQLQuery from '../GraphQLQuery';
 import BroadcastDetail from '../BroadcastDetail/BroadcastDetail';
 import TopicDetail from './TopicDetail';
-import helpers from '../../helpers';
 import { getTopicByIdQuery } from '../../graphql';
 
 function isBroadcast(type) {
@@ -24,17 +22,9 @@ const TopicDetailContainer = props => (
         variables={{id: props.match.params.topicId }}
         displayPager={false}
       > 
-      {(res) => {
-        const topic = res.topic;
-        return (
-          <div>
-            <PageHeader>
-              {topic.name}
-            </PageHeader>
-            <p>{topic.__typename}</p>
-          </div>
-        );
-      }}
+      {res => isBroadcast(res.topic.type)
+        ? <BroadcastDetail topic={res.topic} />
+        : <TopicDetail topic={res.topic} />}
     </GraphQLQuery>
   </Grid>
 );
