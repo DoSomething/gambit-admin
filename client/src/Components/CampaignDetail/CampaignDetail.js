ce59e2c13d6bf2e3d1049183ba310d42a2010810
Aaron Schachter <aaronschachter@gmail.com>
2019-01-28 21:01:36 -0800

    Adds WebSignupConfirmation

--- client/src/Components/CampaignDetail/CampaignDetail.js
+++ client/src/Components/CampaignDetail/CampaignDetail.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import { Col, Grid, PageHeader, Panel, Row, Table } from 'react-bootstrap';
+import { Col, PageHeader, Panel, Row, Table } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 import CampaignTopicList from './CampaignTopicList';
 import TriggerList from '../TriggerList/TriggerListContainer';
 import TopicLink from '../TopicLink';
+const config = require('../../config');
 
 const CampaignDetail = (props) => {
   const campaign = props.campaign;
   const triggers = props.conversationTriggers.filter((item) => {
     return item.topic && item.topic.campaign && item.topic.campaign.id === props.campaign.id;
   });
+  const webSignupConfirmation = props.webSignupConfirmations
+    .find(item => item.campaignId === campaign.id);
+
   return (
-    <Grid>
+    <div>
       <PageHeader>
         {campaign.internalTitle}
       </PageHeader>
       <Panel>
         <Panel.Body>
           <p>
-          {campaign.endDate ? <span>Ends {campaign.endDate}</span> : <span>(No end date)</span>}
+          {campaign.endDate
+            ? (
+              <span>
+                Ends <Moment format={config.dateFormat}>{campaign.endDate}</Moment>
+              </span>
+            )
+            : <span>(No end date)</span>}
           </p>
         </Panel.Body>
       </Panel>
@@ -34,7 +45,29 @@ const CampaignDetail = (props) => {
           ))}
         </tbody>
       </Table>
-    </Grid>
+      <h3>Web Signup Confirmation</h3>
+      <Table>
+        <tbody>
+          {webSignupConfirmation
+            ? (
+              <Row componentClass="tr">
+                <Col componentClass="td" md="2" />
+                <Col componentClass="td" md="6">{webSignupConfirmation.text}</Col>
+                <Col componentClass="td" md="4">
+                  <TopicLink topic={webSignupConfirmation.topic} />
+                </Col>
+              </Row>
+            )
+            : (
+              <Row componentClass="tr">
+                <Col componentClass="td">
+                  (No web signup confirmation)
+                </Col>
+              </Row>
+            )}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 
--- client/src/Components/CampaignDetail/CampaignDetailContainer.js
+++ client/src/Components/CampaignDetail/CampaignDetailContainer.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Grid, PageHeader} from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import CampaignDetail from './CampaignDetail';
 import GraphQLQuery from '../GraphQLQuery';
 import { getCampaignDetailByCampaignIdQuery } from '../../graphql';
 
 const CampaignDetailContainer = props => (
-  <GraphQLQuery
-    query={getCampaignDetailByCampaignIdQuery}
-    variables={{id: Number(props.match.params.campaignId) }}
-    displayPager={false}
-  >
-    {res => (
-      <CampaignDetail
-        campaign={res.campaign}
-        conversationTriggers={res.conversationTriggers}
-      />
-    )}
-  </GraphQLQuery>
+  <Grid>
+    <GraphQLQuery
+      query={getCampaignDetailByCampaignIdQuery}
+      variables={{id: Number(props.match.params.campaignId) }}
+      displayPager={false}
+    >
+      {res => (
+        <CampaignDetail
+          campaign={res.campaign}
+          conversationTriggers={res.conversationTriggers}
+          webSignupConfirmations={res.webSignupConfirmations}
+        />
+      )}
+    </GraphQLQuery>
+  </Grid>
 );
   
 CampaignDetailContainer.propTypes = {
